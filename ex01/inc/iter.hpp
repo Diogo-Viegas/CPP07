de0a366448a802bf9bc34558b5756de825db699a
Diogo Viegas <dviegas@c2r3s12.42lisboa.com>
2026-06-29 16:40:03 +0100

    ex01 fix

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -4,19 +4,9 @@
 template <typename A, typename F>
 void iter(A* array, size_t lenght, F function)
 {
-    if(!array || !function)
+    if(!array)
         return;
     for(size_t i = 0; i < lenght; i++)
-    {
-        function(array[i]);
-    }
-}
-template <typename A, typename F>
-void iter(const A *array, size_t lenght,const F function)
-{
-    if(!array || !function)
-        return;
-    for (size_t i = 0; i < lenght; i++)
     {
         function(array[i]);
     }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,29 +1,72 @@
 #include "../inc/iter.hpp"
+#include <iostream>
+#include <string>
+
 
 template <typename T>
 void printElement(const T &element) {
-    std::cout << element << " " << std::endl;
+    std::cout << element << " ";
 }
 
-void mult(int &i)
-{
-    i*=10;
+
+void multPorDez(int &i) {
+    i *= 10;
+}
+
+
+void incrementaFloat(float &f) {
+    f += 1.5f;
 }
 
 int main()
 {
-    std::cout << "Testing INT array non-const" << std::endl;
-    int int_arr[] = {1, 2, 3};
-    ::iter(int_arr, 3, mult);
-    for(int i = 0; i < 3; i++)
-    {
-        std::cout << i <<"-> " << i << " -> " << int_arr[i] << std::endl;
-    }
-    std::cout << "Testing INT array const" << std::endl;
-    const int int_const_arr[] = {1, 2, 3};
-    ::iter(int_const_arr,3,printElement<int>);
-    std::cout << "Testing STRING array const" << std::endl;
-    const std::string str_arr[] = {"Hello", "World", "42"};
-    ::iter(str_arr,3,printElement<std::string>);
-    return (0);
+    std::cout << "=== Teste 1: Array de INT (Nao-Const) - Modificacao ===" << std::endl;
+    int int_arr[] = {1, 2, 3, 4, 5};
+    size_t int_len = sizeof(int_arr) / sizeof(int_arr[0]);
+
+    std::cout << "Original: ";
+    ::iter(int_arr, int_len, printElement<int>);
+    std::cout << std::endl;
+
+    ::iter(int_arr, int_len, multPorDez);
+
+    std::cout << "Apos multPorDez: ";
+    ::iter(int_arr, int_len, printElement<int>);
+    std::cout << "\n" << std::endl;
+
+    
+    std::cout << "=== Teste 2: Array de INT (Const) - Apenas Leitura ===" << std::endl;
+    const int int_const_arr[] = {10, 20, 30};
+    size_t const_len = sizeof(int_const_arr) / sizeof(int_const_arr[0]);
+
+    std::cout << "Array Constante: ";
+
+    ::iter(int_const_arr, const_len, printElement<int>);
+    std::cout << "\n" << std::endl;
+
+   
+    std::cout << "=== Teste 3: Array de FLOAT (Nao-Const) ===" << std::endl;
+    float float_arr[] = {1.0f, 2.0f, 3.0f};
+    size_t float_len = sizeof(float_arr) / sizeof(float_arr[0]);
+
+    std::cout << "Original: ";
+    ::iter(float_arr, float_len, printElement<float>);
+    std::cout << std::endl;
+
+    ::iter(float_arr, float_len, incrementaFloat);
+
+    std::cout << "Apos incremento: ";
+    ::iter(float_arr, float_len, printElement<float>);
+    std::cout << "\n" << std::endl;
+
+  
+    std::cout << "=== Teste 4: Array de STRINGS (Const) ===" << std::endl;
+    const std::string str_arr[] = {"Ola", "Mundo", "42", "Lisboa"};
+    size_t str_len = sizeof(str_arr) / sizeof(str_arr[0]);
+
+    std::cout << "Strings: ";
+    ::iter(str_arr, str_len, printElement<std::string>);
+    std::cout << "\n" << std::endl;
+
+    return 0;
 }
