069ab15299d138d9616a60916957391520832c75
Diogo Viegas <dviegas@c1r9s5.42lisboa.com>
2026-07-10 11:57:27 +0100

    COF start ex02

--- ex02/inc/Array.hpp
+++ ex02/inc/Array.hpp
@@ -5,13 +5,63 @@ template <typename T>
 class Array
 {
     private:
-        T *_arr;
+        T *_elements;
         unsigned int _size;
 
     public:
-        Array();
-        Array(const Array &copy);
-        Array& operator=(const Array *other);
-        ~Array();
+        Array() : _elemnents(NULL), _size(0){}
+        // 2. Construction with an unsigned int n: Creates an array of n elements initialized by default.
+    // Note: Using `new T[n]()` ensures value-initialization (e.g., ints
+        Array(unsigned int n) : _size(n)
+        {
+            if(size > 0)
+            {
+                _elements = new T[size]();
+            }
+            else
+            {
+                _elements = NULL;
+            }
+        }
+        Array(const Array &copy)
+        {
+            if(_size > 0)
+            {
+                _elements = T[_size];
+                for(unsigned int = 0; i < _size;i++)
+                {
+                    _elements[i] = other._elements[i]; 
+                }
+            }
+            else
+            {
+                _elements = NULL;
+            }
+       
+        }
+        Array& operator=(const Array *other)
+        {
+            if(this != &other)
+                delete[] _elements
+            _size = other._size;
+            if(_size > 0)
+            {
+                _elements = T[_size];
+                for(unsigned int i = 0; i < _size; i++)
+                {
+                    _elements[i] = other->_elements[i];
+                }
+            }
+            else
+            {
+                _elements = NULL;
+            }
+            return *this
+        }
+        ~Array()
+        {
+            delete[] _elements;
+        }
+        
 };
 #endif
